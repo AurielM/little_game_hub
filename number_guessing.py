dddf64dc0e383b55385ac9042313748d6bcc1d7e
amcshaw <auriel.mcshaw@oxehealth.com>
2023-01-12 15:54:02 +0000

    class initialised and imports commenced

---
+++ number_guessing.py
@@ -0,0 +1,13 @@
+from random import randint
+
+class RandomNumberG:
+    """
+    In this game you need to guess the number chosen by the computer between 1 and 100
+    Depending on the level of difficulty chosen you'll get 5, 10 or 15 guesses
+    Guess the number before your chances run out and you win the round!
+
+    You'll have the option of returning back to the main menu after each run
+    """
+
+    def __init__(self, player):
+        pass
