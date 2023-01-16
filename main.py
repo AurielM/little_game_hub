e42f9c81f620db4db9c5a90b9c6e899b0f079371
amcshaw <auriel.mcshaw@oxehealth.com>
2023-01-16 16:22:57 +0000

    inquirer introduced to pick game

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
-
+import inquirer
 
 class Hub:
     """
-    
+    This will provide the menu of games available to be played.
     """
 
-    def __init__(self, location, player_score, player_name):
-        Hub.location = location 
-        Hub.player_score = player_score
-        Hub.player_name = player_name
+    def __init__(self):
+        self.location = 'The little_game_hub' 
+        self.player_score = 0
+        self.player_name = None
 
-    def introduce_player():
+    def introduce_player(self):
         
-        player_name = input('\nWelcome player! \n\n To avoid being called \'player\' for the remainder of the game input name here: ')
+        self.player_name = input('\nTo avoid being called \'player\' for the remainder of the game input your name here: ')
         
-        if player_name == '':
-            player_name = 'player'
+        if self.player_name == '':
+            self.player_name = 'player'
 
-        print(f'ok! Your name is {player_name}')
+        print(f'ok! Your name is {self.player_name}')
+
+    def welcome_message(self):
+        print(f'\n Welcome to the {self.location}, {self.player_name}!')
+
+    def main_menu(self):
+        questions = [
+            inquirer.List('Choice of Game',
+                message="What game would you like to play? \n To exit game: 'Ctrl + c'.",
+                choices=['Number Guessing: undergoing maintenance', 'Hangman: undergoing maintenance', 'Rock Paper Scissors: undergoing maintenance', 'Other'],
+                carousel=True
+            ),]
+        answers = inquirer.prompt(questions)
+        return(answers)
 
 
-Hub.introduce_player()
+game = Hub()
+game.introduce_player()
+game.welcome_message()
+game.main_menu()
