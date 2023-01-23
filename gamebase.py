96150991df393e36d0c300613142f31336350014
amcshaw <auriel.mcshaw@oxehealth.com>
2023-01-23 14:21:21 +0000

    correcting spelling on players choice variable

--- gamebase.py
+++ gamebase.py
@@ -22,8 +22,8 @@ class GameBase:
         stay_or_nay = [
                     inquirer.Confirm("Continue", message="Play another round?", default=False
                     ),]
-        dcit_player_choice = inquirer.prompt(stay_or_nay)
-        if dcit_player_choice['Continue']:
+        dict_player_choice = inquirer.prompt(stay_or_nay)
+        if dict_player_choice['Continue']:
             return True
         else:
             return False
